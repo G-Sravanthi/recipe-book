673890440caac23134cec02603351fe7d4fa3bd6
JONATHON LUCKETT <jluckettt@gmail.com>
2018-01-30 13:41:20 -0700

    landing page complete

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -17,19 +17,29 @@ class LandingPage extends Component {
     let landing = null
     if(this.state.visited) {
       landing = (
-        <Aux>
+        <div styl={{display: 'block'}}>
           <Link to='/recipe-list'>
             <Button className={classes.Links} bsSize="large" bsStyle="link"><strong>Recipe List</strong></Button>
           </Link>
           <Link to='/build-recipe'>
             <Button className={classes.Links} bsSize="large" bsStyle="link"><strong>Build Recipe</strong></Button>
           </Link>
-        </Aux>
+        </div>
       )
     }
     return (
       <main className={classes.Landing}>
-        {landing}
+        <div className={classes.First}>Create a Recipe</div>
+        <div className={classes.Second}>Discover a Recipe</div>
+        <div className={classes.Third}>Eat and Enjoy
+          {landing}
+        </div>
+        <div className={classes.FirstMobile}>Create</div>
+        <div className={classes.SecondMobile}>Discover</div>
+        <div className={classes.ThirdMobile}>Eat</div>
+        <div className={classes.FourthMobile}>Enjoy
+          {landing}
+        </div>
       </main>
     )
   }
